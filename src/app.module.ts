a29f533e42b4bceabb3eedcd25262daa706db236
alxxr <alexx47724@gmail.com>
2024-03-07 01:14:51 -0500

    Se creo la arquitectura hexagonal

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { HttpModule } from '@nestjs/axios';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { MessageController } from './message/message.controller';
+import { AppController } from './interfaces/controllers/app.controller';
+import { AppService } from './application/services/app.service';
+import { MessageController } from './interfaces/controllers/message/message.controller';
 
 @Module({
   imports: [
