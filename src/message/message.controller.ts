3d18bf353179cd4c6736e3cc38331caeb7c4d96f
alxxr <alexx47724@gmail.com>
2024-03-06 23:17:13 -0500

    Actualizacion del BFF

--- src/message/message.controller.ts
+++ src/message/message.controller.ts
@@ -1,19 +1,28 @@
 import { Controller, Post, Body } from '@nestjs/common';
-import { firstValueFrom } from 'rxjs';
 import { HttpService } from '@nestjs/axios';
 
 @Controller('message')
 export class MessageController {
-    constructor(private httpService: HttpService) { }
+  constructor(private httpService: HttpService) { }
 
-    @Post('send')
-    async sendMessage(@Body() data: any) {
-        try {
-            // Aquí puedes enviar los mensajes al microservicio de Python
-            const response = await firstValueFrom(this.httpService.post('http://direccion_del_microservicio', data));
-            return response.data;
-        } catch (error) {
-            throw new Error('Error al enviar los mensajes al microservicio de Python');
-        }
+  @Post('send')
+  async sendMessage(@Body() data: any) {
+
+    const requestData = {
+        prospects: data.prospects.map(prospect => ({
+            prospect: prospect.prospect,
+            messages: prospect.messages,
+        })),
+    };
+    try {
+        const requestDataJson = JSON.stringify(requestData);
+        const response = await this.httpService.post('http://127.0.0.1:8000/analyze_sentiment/', requestDataJson, {headers: {'Content-Type': 'application/json',}}).toPromise();
+        return response.data;
+    
+    } catch (error) {
+
+      throw new Error('Error al enviar los mensajes al microservicio de FastAPI');
+
     }
+  }
 }
